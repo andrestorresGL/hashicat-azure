a9d955af4e72fd7e4a96af4ac49ec2e7d97683d9
Andres Torres <andres.torresduran@gmail.com>
2022-06-14 19:44:29 +0000

    Added network module

---
+++ network.tf
@@ -0,0 +1,6 @@
+module "network" {
+  source  = "app.terraform.io/andres2-org/network/azurerm"
+  version = "3.5.0"
+  resource_group_name = resource_group_name.myresourcegroup
+  # insert required variables here
+}
